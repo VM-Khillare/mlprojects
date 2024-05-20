7f4d762471b40d4303aa752dc43a673241cc833c
Vishal M Khillare <khillare.vishal@gmail.com>
2024-05-20 19:29:03 +0530

    EDA and Problem Statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
 #import logging
+#from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     __,__,exc_tb=error_detail.exc_info()
@@ -13,14 +14,13 @@ def error_message_detail(error,error_detail:sys):
 
 class CustomException(Exception):
     def __init__(self,error_message, error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
 
 
     def __str__(self):
         return self.error_message
-
-## Below code for testing the exception file
+    
 #if __name__=="__main__":
 
 #    try:
