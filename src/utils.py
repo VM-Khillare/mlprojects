fe6478cc7aea99a08ff59999100a0cceceb7e3ac
Vishal M Khillare <khillare.vishal@gmail.com>
2024-05-24 13:28:18 +0530

    Hyper Parameter Training

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,8 @@ import pickle
 
 import pandas as pd
 import numpy as np
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -19,3 +21,35 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+def evaluate_model(X_Train,y_train,x_test,y_test,models,param):
+    try:
+        report ={}
+
+        for i in range (len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            # Model Training using grid search
+            gs = GridSearchCV(model,para,cv=3) # n_jobs=n_jobs,verbose=verbose,refit=refit)
+            gs.fit(X_Train,y_train)            
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_Train,y_train)
+                
+            #model.fit(X_Train,y_train)  #Train Model
+
+            y_train_pred = model.predict(X_Train)
+
+            y_test_pred = model.predict(x_test)
+
+            train_model_score = r2_score(y_train,y_train_pred)
+
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+    
+    except Exception as e:
+        raise CustomException(e,sys)
